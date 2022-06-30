68c7c4d94fa616909862ec30c611595fa9a1548d
Artur Todeschini <artodeschini@gmail.com>
2022-06-30 08:33:59 -0300

    delete de categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -19,21 +19,36 @@ router.post("/categories/save", (req, res) => {
     } else {
         res.redirect("/admin/categories/new");
     }
-    //res.send("rota de categorias")
 });
 
 router.get("/admin/categories/new", (req, res) => {
-    // res.send("rota de categorias novas");
     res.render("admin/categories/new");
 });
 
 router.get("/admin/categories", (req, res) => {
-
     Category.findAll().then(categories => {
         res.render("admin/categories/index", {
             categories: categories
         });
     });    
+});
+
+// router.delete('/categories/:id', (req, res) => {
+router.post('/categories/delete', (req, res) => {
+    let id = req.body.id;
+    if (id != undefined) {
+        if (!isNaN(id)) { // verifica se é um numero numero
+            Category.destroy({
+                where: {id: id}
+            }).then(() => {
+                res.redirect("/admin/categories");
+            }); 
+        } else {
+            res.redirect("/admin/categories");
+        }
+    } else {
+        res.redirect("/admin/categories");
+    }
 });
 
 module.exports = router;
