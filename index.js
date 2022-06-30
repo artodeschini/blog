4a0e40100c7b8741a4ca812e471ae46d7ff6e81a
Artur Todeschini <artodeschini@gmail.com>
2022-06-30 16:16:27 -0300

    home passo 1

--- index.js
+++ index.js
@@ -33,8 +33,28 @@ app.use(express.static('public'));
 
 // routes
 app.get("/",(req, res) => {
+    Article.findAll({order: [['id', 'DESC']]}).then(articles => {
+        res.render("index", {articles: articles});
+    });
     //res.send("is working");
-    res.render("index");
+    // res.render("index");
+});
+
+app.get("/:slug",(req, res) => {
+    let slug = req.params.slug;
+    Article.findOne({where: {slug: slug}}).then(article => {
+        if (article != undefined) {
+            res.render("article", {article: article});
+        } else {
+            res.redirect("/");
+        }
+        
+    }).catch( error => {
+        console.log(error);
+        res.redirect("/");
+    });
+    //res.send("is working");
+    // res.render("index");
 });
 
 app.use("/", categoriesController);
